875bfe3c7ab338efa6a4dbe1a12223db8a486ac3
Kalciumm <72413142+Kalciumm@users.noreply.github.com>
2022-10-05 13:44:04 -0400

    workpls

---
+++ GED_Base-Lab4/Assets/_Scripts/Singleton/Singleton.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton : MonoBehaviour
+{
+    
+    SpikeBall spike2 = new SpikeBall(item, new RedMat());
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
